96824fe8c6ec5648326ebcbe611780e43e9d8a22
yuki.nagashima <yu.nagashima0402@gmail.com>
2017-04-25 05:28:08 +0000

    revenge task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @task = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -24,12 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-     @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
     if @task.update(task_params)
       flash[:success] = 'タスク は正常に更新されました'
       redirect_to @task
@@ -40,7 +38,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = 'タスク は正常に削除されました'
@@ -50,6 +47,9 @@ end
 
 
   private
+  def set_task
+    @task = Task.find)(pa[:id])
+  end
 
   # Strong Parameter
   def task_params
